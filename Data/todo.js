d16d2f79ceb93f4e67217cb71798957bd93d7c83
ajay-3 <ajay.hn3@gmail.com>
2020-01-09 02:29:23 +0530

    first Commit

---
+++ Data/todo.js
@@ -0,0 +1,3 @@
+var tasks = [];
+
+module.exports = tasks;
---
+++ server.js
@@ -0,0 +1,98 @@
+var express = require('express');
+var bodyParser = require("body-parser");
+var tasks = require('./Data/todo');
+
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('port',(process.env.PORT||3000));
+
+app.get("/",(req,res)=>{
+    res.json("Hi there welcome to app");
+})
+app.post('/addtodo',(req,res)=>{
+     res.sendStatus(200);
+     var newTask = req.body.text;
+     tasks.push(newTask);
+     let data = {
+        "blocks": [
+            {
+                "type": "section",
+			    "fields":[{
+                        "type": "plain_text",
+                        "text": "ADDED TODO for \" "  + req.body.text + "\""
+                    }
+                ]
+            }
+        ]};
+     res.json(data);
+});
+
+app.post('/marktodo', (req,res)=>{
+     res.sendStatus(200);
+     var deletetask = req.body.text;
+     if(indexof(deletetask)){
+        tasks.splice(indexof(deletetask),1);
+        let data = {
+            "blocks": [
+                {
+                    "type": "section",
+                    "fields":[{
+                            "type": "plain_text",
+                            "text": "Removed TODO for \" "  + req.body.text + "\""
+                        }]
+                }
+            ]};
+        res.json(data)
+     }else{
+        let data={
+            "blocks":[
+                {
+                    "type" : "section",
+                    "fields" : [{
+                        "type" : "plain_text",
+                        "text" : "Please enter proper To-do item"
+                    }]
+                }
+            ]
+        }
+        res.json(data)
+     }
+});
+
+app.get('/listtodos',(req,res)=> {
+  if(tasks.length){
+    var string = tasks.toString().split(",").join(" ");
+    let data={
+     "blocks":[
+         {
+             "type" : "section",
+             "fields" : [{
+                 "type" : "plain_text",
+                 "text" : string
+             }]
+         }
+        ]
+        }
+        res.json(data);
+       }else{
+        let data={
+            "blocks":[
+                {
+                    "type" : "section",
+                    "fields" : [{
+                        "type" : "plain_text",
+                        "text" : "NO TODOS"
+                    }]
+                }
+               ]
+               }
+               res.json(data)
+        }
+    }
+   );
+
+app.listen(app.get('port'), ()=> {
+  console.log('Server is running at',app.get('port'));
+});
