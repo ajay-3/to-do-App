c0303442f85f127a25c3c09dd40c0e9f38b4b067
ajay-3 <ajay.hn3@gmail.com>
2020-01-09 12:11:40 +0530

    fourth commit

--- server.js
+++ server.js
@@ -12,28 +12,29 @@ app.get("/",(req,res)=>{
     res.json("Hi there welcome to app");
 })
 app.post('/addtodo',(req,res)=>{
-    console.log("A addtoDo request has been request remotely");
-    console.log(req.body);
-     var newTask = req.body.text;
-     tasks.push(newTask);
-     let data = {
-        "blocks": [
-            {
-                "type": "section",
-			    "fields":[{
-                        "type": "plain_text",
-                        "text": "ADDED TODO for \" "  + req.body.text + "\""
-                    }
-                ]
-            }
-        ]};
-     res.json(data);
+    res.json({"text":"200"});
+    // // console.log("A addtoDo request has been request remotely");
+    // // console.log(req.body);
+    //  var newTask = req.body.text;
+    //  tasks.push(newTask);
+    //  let data = {
+    //     "blocks": [
+    //         {
+    //             "type": "section",
+	// 		    "fields":[{
+    //                     "type": "plain_text",
+    //                     "text": "ADDED TODO for \" "  + req.body.text + "\""
+    //                 }
+    //             ]
+    //         }
+    //     ]};
+    //  res.json(data);
 });
 
 app.post('/marktodo', (req,res)=>{
     console.log("this is before sending response");
-     res.sendStatus(200);
     console.log("A marktodo request has been request remotely");
+    res.sendStatus(200);
     console.log(req.body);
      var deletetask = req.body.text;
      if(indexof(deletetask)){
@@ -65,36 +66,41 @@ app.post('/marktodo', (req,res)=>{
      }
 });
 
-app.get('/listtodos',(req,res)=> {
-  console.log("the list has been called remotely");
-  if(tasks.length){
-    var string = tasks.toString().split(",").join("  ");
-    let data={
-     "blocks":[
-         {
-             "type" : "section",
-             "fields" : [{
-                 "type" : "plain_text",
-                 "text" : string 
-             }]
-         }
-        ]
-        }
-        res.json(data);
-       }else{
-        let data={
-            "blocks":[
-                {
-                    "type" : "section",
-                    "fields" : [{
-                        "type" : "plain_text",
-                        "text" : "NO TODOS"
-                    }]
-                }
-               ]
-               }
-               res.json(data)
-        }
+app.get('/listtodos',async (req,res)=> {
+    try{ 
+    console.log("the list has been called remotely");
+    if(tasks.length){
+      var string = tasks.join("\n");
+      let data={
+       "blocks":[
+           {
+               "type" : "section",
+               "fields" : [{
+                   "type" : "plain_text",
+                   "text" : {
+                       "type":"mrkdwn",
+                        "text":string
+                      } 
+               }]
+           }
+          ]
+          }
+          res.json(data);
+         }else{
+          let data={
+              "blocks":[
+                  {
+                      "type" : "section",
+                      "fields" : [{
+                          "type" : "plain_text",
+                          "text" : "NO TODOS"
+                      }]
+                  }
+                 ]};
+                res.json(data);
+          }}catch(error){
+              res.json({"text":error});
+          };
     }
    );
 
